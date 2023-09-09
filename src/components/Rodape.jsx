95c026529fd3e394e2fcd500a04280d629d5987f
rafaelknunes <rafaelknunes@gmail.com>
2023-09-09 15:25:08 -0300

    muda rodape e layouts

--- src/components/Rodape.jsx
+++ src/components/Rodape.jsx
@@ -3,15 +3,9 @@ import { Link } from "react-router-dom";
 export default function Rodape() {
     return (
         <footer>
-            <div class="footer-content">
-                <div class="footer-center">
-                    <p>Copyright © 2023 de Grupo Hall 9000</p>
-                </div>
-
-                <div class="footer-logo">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/f/f6/HAL9000.svg" alt="Logo Grupo HAL 9000"/>
-                
-                </div>
+            <div className="footer-center">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/f/f6/HAL9000.svg" alt="Logo Grupo HAL 9000" className="footer-logo-image" />
+                <p className="footer-text">Copyright © 2023 de Grupo Hal9000</p>
             </div>
         </footer>
     )
