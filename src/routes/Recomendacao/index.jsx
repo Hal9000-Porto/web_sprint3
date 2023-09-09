49ba55357ebcab91c826993a0f1f8dd117355854
rafaelknunes <rafaelknunes@gmail.com>
2023-09-09 15:04:17 -0300

    inclui opções dos estados

--- src/routes/Recomendacao/index.jsx
+++ src/routes/Recomendacao/index.jsx
@@ -53,19 +53,37 @@ export default function Recomendacao() {
                     <option value="Roda travada">Roda travada</option>
                 </select>
 
-                <label for="marcaCaminhao">Marca do Caminhão:</label>
-                <select id="marcaCaminhao" name="marcaCaminhao">
-                        <option value="DAF">DAF</option>
-                        <option value="Ford">Ford</option>
-                        <option value="Iveco">Iveco</option>
-                        <option value="Mercedez">Mercedes</option>
-                        <option value="Scania">Scania</option>
-                        <option value="Volvo">Volvo</option>
-                        <option value="Volvo">Volkswagen</option>
-                        <option value="Volvo">MAN</option>
-                        <option value="Volvo">Hyundai</option>
+                <label for="estado">Estado:</label>
+                <select id="estado" name="estado">
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
                 </select>
-                
+
                 <label for="cidade">Cidade:</label>
                 <input type="text" id="cidade" name="cidade" required/>
 
