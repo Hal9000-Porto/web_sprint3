e4c63d358b26a7e9f56be8924ba81404395f165a
Diogo-gallina <diogogallina20@gmail.com>
2023-09-11 08:11:34 -0300

    chore: identetion membros route

--- src/routes/Membros/index.jsx
+++ src/routes/Membros/index.jsx
@@ -1,161 +1,47 @@
 import React from 'react';
 
- 
-
-// Crie um array com informações de 5 membros da equipe
-
- 
-
 const membrosDaEquipe = [
 
- 
-
-    //membro 1
-
- 
-
     {
-
- 
-
         nome: 'Diogo Gallina (98605)',
-
-
         foto: '../../src/assets/diogo.jpg',
-
     },
 
- 
-
-    //membro 2
-
- 
-
     {
-
- 
-
         nome: 'João Dubas Leal Kleye - (76153)',
-
- 
-
         foto: '../../src/assets/dubas.jpg',
-
- 
-
-
-
- 
-
     },
 
- 
-
-    //membro 3
-
- 
-
     {
-
- 
-
         nome: 'Pedro Henrique Archilha (550450)',
-
- 
-
         foto: '../../src/assets/pedro.png',
-
-
-
     },
 
- 
-
-    //membro 4
-
- 
-
     {
-
- 
-
         nome: 'Pedro Henrique Lourenço (98402)',
-
- 
-
-        foto: '../../src/assets/henrique.png',
-
- 
-
- 
-
+        foto: '../../src/assets/henrique.png',    
     },
-
- 
-
-    //membro 5
-
- 
 
     {
-
- 
-
         nome: 'Rafael Klanfer Nunes (99791)',
-
- 
-
-        foto: '../../src/assets/rafa.jpg',
-
- 
-
- 
-
+        foto: '../../src/assets/rafa.jpg',    
     },
-
- 
-
 ];
-
- 
-
- 
-
- 
 
 export default function Membros() {
 
- 
-
     return (
-
- 
-
         <div className="membros">
-
- 
 
             {membrosDaEquipe.map((membro, index) => (
 
- 
-
                 <div className="bloco-criadores" key={index}>
-
- 
-
                     <div className="perfil">
-
- 
-
                         <img src={membro.foto} alt={`Foto do colaborador ${membro.nome}`} className="img-perfil" />
                         <p>{membro.nome}</p>
-                        </div>
-
                     </div>
+                </div>
             ))}
         </div>
-
- 
-
     );
 }
